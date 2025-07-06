30252093f8ff6d29dbec8b34dcc214d9bb0622cf
Daniel Saravia <saraviad4444@gmail.com>
2025-07-06 01:33:42 -0700

    rotate

---
+++ ReverseLearn.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+public class ReverseLearn {
+    static void reverse(int[] a) {
+        int i = 0, j = a.length - 1;
+        while(i < j){
+            int temp = a[i];
+            a[i++] = a[j];
+            a[j--] = temp;
+        }
+    }
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5, 6};
+
+        System.out.println(Arrays.toString(a));
+
+        reverse(a);
+
+        System.out.println(Arrays.toString(a));
+
+    }
+    
+}
---
+++ Stringre.java
@@ -0,0 +1,9 @@
+public class Stringre {
+    public static void main(String[] args) {
+        String s = "ILoveTiwi";
+        String rev = new StringBuilder(s).reverse().toString();
+        System.out.println(rev);
+
+    }
+    
+}
